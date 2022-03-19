5ceb5fe6237ed4f33eae5cfb480ced6bd28f1ae0
rpaladin <69180012+rpaladin@users.noreply.github.com>
2022-03-19 16:52:28 -0700

    Add 404 handling, cleanup redundancy, whitespace

--- Localhost_&_Redirects.py
+++ Localhost_&_Redirects.py
@@ -1,26 +1,22 @@
-import http.server
-import socketserver
-
+import http.server, socketserver
 IP = "localhost"
 PORT = 80
 sections = ["/news", "/community", "/fund", "/notes", "/download"]
-
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == "" or self.path == "/":
             self.path = "index.html"
-        for s in sections:
-            if self.path == s:
-                self.path = self.path + ".html"
+        elif self.path in sections:
+            self.path += ".html"
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-if __name__ == "__main__": 
-    handler_object = MyHttpRequestHandler
-    my_server = socketserver.TCPServer((IP, PORT), handler_object)
-
+    def send_error(self, code, message=False):
+        if code == 404:
+            self.path = "404.html"
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+if __name__ == "__main__":
+    web_server = socketserver.TCPServer((IP, PORT), HTTPRequestHandler)
     try:
-        my_server.serve_forever()
+        web_server.serve_forever()
     except KeyboardInterrupt:
         pass
-
-    my_server.server_close()
+    web_server.server_close()
