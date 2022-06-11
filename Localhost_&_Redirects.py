a222a2f2b1178907d90b0d3d81a77433ed01327e
RPaladin <69180012+rpaladin@users.noreply.github.com>
2022-06-10 18:09:53 -0700

    Remove deprecated fund page

--- Localhost_&_Redirects.py
+++ Localhost_&_Redirects.py
@@ -1,7 +1,7 @@
 import http.server, socketserver
 IP = "localhost"
 PORT = 80
-sections = ["/news", "/community", "/fund", "/notes", "/download"]
+sections = ["/news", "/community", "/notes", "/download"]
 class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == "" or self.path == "/":
